98446e40b806a7d8c18c321aefcc594f9a470f23
EdChiu-tech <edward_chiu604@gmail.com>
2021-05-30 18:18:13 -0700

    updated js for bio and shows, styled pages

--- sprint-two/scripts/build-shows-page.js
+++ sprint-two/scripts/build-shows-page.js
@@ -22,7 +22,7 @@ const footer = document.querySelector("footer")
 // Shows Header
 
 const showsHeader= newElementClass("h2", "shows__header")
-showsHeader.innerText= "Show"
+showsHeader.innerText= "Shows"
 shows.append(showsHeader);
 
 // Shows array
@@ -32,31 +32,37 @@ let showsArray = [
         dates: "Mon Sept 06 2021",
         venue: "Ronald Lane",
         location: "San Francisco, CA",
+        class: "object--show",
      },
      {
         dates: "Tue Sept 21 2021",
         venue: "Pier 3 East",
         location: "San Francisco, CA",
+        class: "object--hidden",
      },
      {
         dates: "Fri Oct 15 2021",
         venue: "View Lounge",
         location: "San Francisco, CA",
+        class: "object--hidden",
     },
     {
         dates: "Sat Nov 06 2021",
         venue: "Hyatt Agency",
         location: "San Francisco, CA",
+        class: "object--hidden",
     },
     {
         dates: "Fri Nov 26 2021",
         venue: "Moscow Center",
         location: "San Francisco, CA",
+        class: "object--hidden",
     },
     {
         dates: "Wed Dec 15 2021",
         venue: "Press Club",
         location: "San Francisco, CA",
+        class: "object--hidden",
     },
 ]
 
@@ -72,32 +78,50 @@ const createCard = (show) =>{
     for (let i = 0; i < show.length; i++){
 
         const showsCard = newElementClass("article", "shows__card");
+
+        let showsDateContainer = newElementClass("div", "shows__container-date");
+        showsCard.append(showsDateContainer)
         
-        let showsDateHeader = newElementClass("h3", "shows__header");
+        let showsDateHeader = newElementClass("h3", "shows__subheader");
+        showsDateHeader.classList.add(show[i].class)
         showsDateHeader.innerText = "DATES"
-        showsCard.append(showsDateHeader);
+        showsDateContainer.append(showsDateHeader);
         
         let showsDate = newElementClass("p", "shows__date");
         showsDate.innerText = show[i].dates;
-        showsCard.append(showsDate);
+        showsDateContainer.append(showsDate);
 
-        let showsVenueHeader = newElementClass("h3", "shows__header");
+        let showsVenueContainer = newElementClass("div", "shows__container-venue");
+        showsCard.append(showsVenueContainer);
+
+        let showsVenueHeader = newElementClass("h3", "shows__subheader");
+        showsVenueHeader.classList.add(show[i].class)
         showsVenueHeader.innerText = "VENUE"
-        showsCard.append(showsVenueHeader);
+        showsVenueContainer.append(showsVenueHeader);
         
         let showsVenue = newElementClass("p", "shows__venue");
         showsVenue.innerText = show[i].venue;
-        showsCard.append(showsVenue);
+        showsVenueContainer.append(showsVenue);
 
-        let showsLocationHeader = newElementClass("h3", "shows__header");
+        let showsLocationContainer = newElementClass("div", "shows__container-location");
+        showsCard.append(showsLocationContainer);
+
+        let showsLocationHeader = newElementClass("h3", "shows__subheader");
+        showsLocationHeader.classList.add(show[i].class)
         showsLocationHeader.innerText = "LOCATION"
-        showsCard.append(showsLocationHeader);
+        showsLocationContainer.append(showsLocationHeader);
 
         let showsLocation = newElementClass("p", "shows__location");
         showsLocation.innerText = show[i].location;
-        showsCard.append(showsLocation);
+        showsLocationContainer.append(showsLocation);
 
-        let showsButton = newElementClass("button","shows__button")
+        let showsButton = newElementClass("button","shows__button");
+
+        showsButton.setAttribute("value", `${show[i].venue}, ${show[i].location}`);
+        showsButton.addEventListener("click", event =>{
+        console.log(event.srcElement.value)
+        })
+        
         showsButton.innerText = "BUY TICKETS"
         showsCard.append(showsButton)
           
@@ -106,3 +130,5 @@ const createCard = (show) =>{
 }
 
 createCard(showsArray);
+
+
--- sprint-two/scripts/index-page.js
+++ sprint-two/scripts/index-page.js
@@ -30,42 +30,58 @@ const footer = document.querySelector("footer")
     const form = newElementClass("form", "form")
     commentDiv.append(form);
     
+    const formAvatarContainer = newElementClass("div", "form__avatar-container")
+    form.append(formAvatarContainer)
+
     const formAvatar = newElementClass("div", "form__avatar")
-    form.append(formAvatar)
+    formAvatarContainer.append(formAvatar)
+
+    const formCommentContainer = newElementClass("div", "form__comment-container")
+    form.append(formCommentContainer)
+
+
+    
 
 // Form - Name  
+
     let labelName = newElementClass("label", "form__header")
-    labelName.innerText = "Name"
+    labelName.innerText = "NAME"
     labelName.setAttribute("for", "userName")
-    form.append(labelName)
+    formCommentContainer.append(labelName)
 
     let inputName = newElementId("input", "form__input")
+    inputName.classList.add("form__field")
     inputName.setAttribute("type", "text")
     inputName.setAttribute("id", "userName")
     inputName.setAttribute("name", "userName")
     inputName.setAttribute("placeholder", "Enter your name")
-    form.append(inputName)
+    formCommentContainer.append(inputName)
 
 // Form - Comments
 
     let labelComm = newElementClass("label", "form__header")
-    labelComm.innerText = "Comment"
+    labelComm.innerText = "COMMENT"
     labelComm.setAttribute("for", "input")
-    form.append(labelComm)
+    formCommentContainer.append(labelComm)
 
     let inputComm = newElementId("input", "form__input")
-    inputComm.setAttribute("type", "text")
+    inputComm.classList.add("form__field")
+    inputComm.setAttribute("type", "comment")
     inputComm.setAttribute("id", "input")
     inputComm.setAttribute("name", "input")
     inputComm.setAttribute("placeholder", "Add a new comment")
-    form.append(inputComm)
+    formCommentContainer.append(inputComm)
+
 
 // Form - Submit button
+
+const formButtonContainer = newElementClass("div", "form__button-container")
+formCommentContainer.append(formButtonContainer);
 
     let submitBtn = newElementId("button", "form__button")
     submitBtn.setAttribute("type", "submit")
     submitBtn.innerText = "COMMENT"
-    form.append(submitBtn)
+    formButtonContainer.append(submitBtn)
 
 //
 
@@ -74,16 +90,19 @@ let commentsArray = [
         name: "Connor Walton",
         date: "02/17/2021",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        avatar: "comment__avatar-default"
      },
      {
         name: "Emilie Beach",
         date: "01/09/2021",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-     },
+        avatar: "comment__avatar-default"
+    },
      {
         name: "Miles Acosta",
         date: "12/20/2020",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        avatar: "comment__avatar-default"
     },
     
 ]
@@ -100,18 +119,30 @@ const createCard = (comments) =>{
     for (let i = 0; i < comments.length; i++){
 
         const commentCard = newElementClass("article", "comment__card");
+
+        let cardTextContainer = newElementClass("div", "comment__text-container")
+        commentCard.append(cardTextContainer);
         
+        let cardUserDateContainer = newElementClass("div", "comment__user-date-container")
+        cardTextContainer.append(cardUserDateContainer)
+
         let cardUser = newElementClass("h3", "comment__user");
         cardUser.innerText = comments[i].name;
-        commentCard.append(cardUser);
+        cardUserDateContainer.append(cardUser);
         
         let cardDate = newElementClass("span", "comment__date");
         cardDate.innerText = comments[i].date;
-        commentCard.append(cardDate);
+        cardUserDateContainer.append(cardDate);
         
         let cardComment = newElementClass("p", "comment__content");
         cardComment.innerText = comments[i].comment;
-        commentCard.append(cardComment);
+        cardTextContainer.append(cardComment);
+
+        let formDefaultAvatarContainer = newElementClass ("div", "comment__avatar-container")
+        cardTextContainer.insertAdjacentElement("beforebegin",formDefaultAvatarContainer)
+
+        let cardAvatar = newElementClass("div",comments[i].avatar);
+        formDefaultAvatarContainer.append(cardAvatar);
         
         commentContainerDiv.append(commentCard);
     }
@@ -119,25 +150,59 @@ const createCard = (comments) =>{
 
 createCard(commentsArray);
 
-//  Grab user input and replace default comments
+// replace default comments
 
 const formContainer = document.querySelector(".form")
+let nameBox = document.getElementById("userName");
+let inputBox = document.getElementById("input");
+
+
+let currentDate = new Date();
 
 const displayComment = (event) =>{
     event.preventDefault();
 
+    let success = false;
+
     commentContainerDiv.innerHTML = "";
+
+    // Check comment box to be empty, either full, both full
+
+    if(nameBox.value !== "" && inputBox.value !== ""){
+        nameBox.classList.remove("form__field--error")
+        inputBox.classList.remove("form__field--error")
+        success = true;
+        
+    }else if (nameBox.value === "" && inputBox.value !==""){
+        nameBox.classList.add("form__field--error")
+        inputBox.classList.remove("form__field--error")
+
     
-    let userComment = {
-        name: event.target.userName.value,
-        date: event.placeholder,
-        comment: event.target.input.value,
+    }else if (inputBox.value === "" && nameBox.value !==""){
+        inputBox.classList.add("form__field--error")
+        nameBox.classList.remove("form__field--error")
+
+    }else{
+        inputBox.classList.add("form__field--error")
+        nameBox.classList.add("form__field--error")
     }
-    commentsArray.unshift(userComment);
-    commentsArray.pop();
+    
+    if (success) {
+
+        let userComment = {
+            name: event.target.userName.value,
+            date: currentDate.toLocaleDateString("en-us", {day: "2-digit", month: "2-digit", year: "numeric"}),
+            comment: event.target.input.value,
+            avatar: "form__avatar",
+        }
+        //  pushes new comment into array, and removes last comment
+        commentsArray.unshift(userComment);
+        commentsArray.pop();
+    }
+    
     createCard(commentsArray);
     formContainer.reset();
+
 }
 
 formContainer.addEventListener("submit", displayComment);
-
